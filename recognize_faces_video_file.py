ed5546b6e4a15e141817f85bb43f1d730cc035e5
Dheeraj Trivedi <dheerajtrivedi@google.com>
2019-02-21 15:31:25 +0530

    Changed video type to XVID

--- recognize_faces_video_file.py
+++ recognize_faces_video_file.py
@@ -105,7 +105,7 @@ while True:
 	# if the video writer is None *AND* we are supposed to write
 	# the output video to disk initialize the writer
 	if writer is None and args["output"] is not None:
-		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		fourcc = cv2.VideoWriter_fourcc(*"XVID")
 		writer = cv2.VideoWriter(args["output"], fourcc, 24,
 			(frame.shape[1], frame.shape[0]), True)
 
